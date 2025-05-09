6a7112993fe57dad27d97953d18d3b848c73ad35
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-09 14:24:44 +0530

    [feat] Add base files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import SiteHeader from './components/layout/SiteHeader.vue'
+</script>
+
+<template>
+  <SiteHeader />
+
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
+</template>
+
+<style scoped>
+/* Global Transition Styles while changing views */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
