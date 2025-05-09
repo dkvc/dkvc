c966b14bd8d52154c2f060797a7ff560d93135ed
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-09 14:24:57 +0530

    [feat] Add types

---
+++ src/types/models.ts
@@ -0,0 +1,8 @@
+export interface TimelineItemData {
+  id: string // A unique ID used for v-for keys
+  project_type: string[]
+  month_year: string
+  project_name: string
+  description: string
+  tags: string[]
+}
