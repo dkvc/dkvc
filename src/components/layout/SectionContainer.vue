bab4bfd8a462da07da475aef85a821fbd03f2783
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-09 14:26:32 +0530

    [feat] Add SectionHeader and SectionContainer

---
+++ src/components/layout/SectionContainer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  // Optional Id, useful for anchor links
+  sectionId?: string
+}>()
+</script>
+
+<template>
+  <section :id="sectionId" class="section-container">
+    <div v-if="$slots.header" class="section-container__header-wrapper">
+      <slot name="header"></slot>
+    </div>
+    <div class="section-container__content_wrapper">
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.section-container {
+  margin-top: -3.75rem; /* Negative margin to counteract padding */
+  margin-bottom: 5rem; /* Space between sections */
+  padding-top: 3.75rem; /* Space for anchor link scrolling offset */
+}
+</style>
---
+++ src/components/layout/SectionHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string
+  level: 1 | 2 | 3 | 4 | 5 | 6
+}>()
+
+const headingTag = `h${props.level}` as const
+</script>
+
+<template>
+  <component :is="headingTag" class="section-header-title">
+    {{ text }}
+  </component>
+</template>
+
+<style scoped>
+.section-header-title {
+  margin-bottom: 3.125rem;
+  font-weight: 500;
+  font-size: 2.5em;
+  text-align: center;
+}
+</style>
