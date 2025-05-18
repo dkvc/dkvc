55da21802e0570d03eafd34124b1c920aadb4d14
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-18 11:41:30 +0530

    [feat] Create HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import AboutCardItem from '@/components/contact/AboutCardItem.vue'
+import ContactCardItem from '@/components/contact/ContactCardItem.vue'
+import SectionContainer from '@/components/layout/SectionContainer.vue'
+import SkillsSection from '@/components/skills/SkillsSection.vue'
+import MajorProjectsCardItem from '@/components/skills/MajorProjectsCardItem.vue'
+
+import { profile } from '@/data/about-data'
+</script>
+
+<template>
+  <div>
+    <main class="content-area">
+      <SectionContainer sectionId="contact">
+        <AboutCardItem cardId="about" v-bind="profile" />
+        <ContactCardItem cardId="contact" />
+        <SkillsSection />
+        <MajorProjectsCardItem cardId="major-projects" />
+      </SectionContainer>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.content-area {
+  position: relative; /* Needed for absolute positioning within */
+  margin: 2.5rem auto 0;
+  padding: 0 1.25rem;
+  max-width: 900px;
+}
+</style>
