c475b27dfa2369dfa2e5cab6353afac1edd4e92d
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-18 11:41:05 +0530

    [chore] Add profile data

---
+++ src/data/about-data.ts
@@ -0,0 +1,7 @@
+export const profile = {
+  firstName: 'Dhanush',
+  lastName: 'Kovi',
+  title: 'Currently engaging with AI, Quantum Computing and Web Development',
+  bio: "<b>Thank you for checking out my site!</b><br>I'm <span class='bio-highlight'>Dhanush Kovi</span> (or <span class='bio-highlight'>dkvc</span>). My work centers on developing and <span class='bio-highlight'>optimizing systems</span>, such as complex <span class='bio-highlight'>AI models</span> or crafting fluid <span class='bio-highlight'>web experiences</span>.<br>Recently, I've also started looking into the field of <span class='bio-highlight'>Quantum Computing.</span>",
+  imageUrl: '/profile/logo_mini.jpg',
+}
