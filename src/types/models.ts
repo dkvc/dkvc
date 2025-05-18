6b146eb74120e2ed1783727693470e1a2119f4ae
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-18 11:32:03 +0530

    [fix] Include new fields in TimelineItemData type

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,8 +1,14 @@
 export interface TimelineItemData {
+  additional_tags?: string[]
+  major?: boolean
+  private?: boolean
   id: string // A unique ID used for v-for keys
   project_type: string[]
   month_year: string
   project_name: string
   description: string
   tags: string[]
+  link?: string
+}
+
 }
