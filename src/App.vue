66856bf42c5b2bd0ae49ee8373d2b2e3758a92a5
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-18 11:31:19 +0530

    [feat] Add site footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import SiteHeader from './components/layout/SiteHeader.vue'
+import SiteFooter from './components/layout/SiteFooter.vue'
 </script>
 
 <template>
@@ -11,6 +12,8 @@ import SiteHeader from './components/layout/SiteHeader.vue'
       <component :is="Component" />
     </Transition>
   </RouterView>
+
+  <SiteFooter />
 </template>
 
 <style scoped>
---
+++ src/components/layout/SiteFooter.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import CardItem from '../card/CardItem.vue'
+import packageInfo from '../../../package.json'
+</script>
+
+<template>
+  <footer class="site-footer">
+    <CardItem cardId="footer" fullborder>
+      <template #header>
+        <div class="site-footer__wrapper">
+          <div class="left">© MIT License</div>
+          <div class="center">You've reached the end of the page.</div>
+          <div class="right">v{{ packageInfo.version }}</div>
+        </div>
+      </template>
+    </CardItem>
+  </footer>
+</template>
+
+<style scoped>
+/* TODO: Fix this! Do not do this! */
+.site-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.site-footer :deep(.card-item) {
+  margin-bottom: 0;
+}
+
+.site-footer :deep(.card-item.full-border-card) {
+  border-color: transparent !important;
+}
+
+.site-footer :deep(.card-item.full-border-card):hover {
+  transform: translateX(0) !important;
+}
+
+.site-footer__wrapper {
+  display: flex;
+  justify-content: space-around;
+  align-items: baseline;
+  gap: 2rem;
+  text-align: center;
+}
+</style>
