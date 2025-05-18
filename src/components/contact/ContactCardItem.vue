fdefb39632a92d463c1292ed3590f023907bfe01
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-05-18 11:38:14 +0530

    [feat] Create dynamic contact card

---
+++ src/components/contact/ContactCardItem.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import CardItem from '../card/CardItem.vue'
+import { contacts } from '@/data/contact-data'
+
+defineProps<{
+  cardId?: string
+}>()
+</script>
+
+<template>
+  <CardItem :cardId="cardId" class="card" fullborder>
+    <template #header> You can also find me on: </template>
+    <template #content>
+      <ul class="contact-list">
+        <li v-for="contact in contacts" :key="contact.id" class="contact-list-item">
+          <a :href="contact.link" target="_blank" rel="noopener noreferrer" class="contact-link">
+            <i v-if="contact.iconClass" :class="contact.iconClass" aria-hidden="true"></i>
+            <span class="contact-name">{{ contact.name }}</span>
+          </a>
+        </li>
+      </ul>
+    </template>
+  </CardItem>
+</template>
+
+<style scoped>
+.contact-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.75rem;
+  margin-top: 1em;
+}
+
+.contact-name {
+  font-weight: 500;
+}
+
+.contact-link {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem; /* Space between icon and text */
+  transition:
+    background-color 0.2s ease-in-out,
+    color 0.2s ease-in-out;
+  box-shadow: 0 0.25rem 0.75rem hsla(0, 0%, 0%, 0.25);
+  border: 0.1rem solid transparent;
+  border-color: hsl(14.39, 100%, 56.67%);
+  border-radius: 4px;
+  padding: 0.5rem 0.5rem;
+  color: var(--text-color);
+  text-decoration: none;
+}
+
+.contact-link:hover,
+.contact-link:focus {
+  background-color: hsl(14.39, 100%, 56.67%);
+}
+
+.contact-link i {
+  width: 1.5em; /* To align text even if some items don't have icons */
+  font-size: 1.2em;
+  text-align: center;
+}
+</style>
