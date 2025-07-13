4e822df74f87871b93afcfa4c651d1629e0093bc
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2025-07-13 15:47:13 +0530

    [chore] Add exp-data

---
+++ src/data/exp-data.ts
@@ -0,0 +1,16 @@
+import type { ExpItemData } from '@/types'
+
+export const expData: ExpItemData[] = [
+  {
+    id: '1',
+    location: 'Remote',
+    position: 'Web Developer Intern',
+    organization: 'Genix AI Healthcare',
+    month_year_start: 'June 2025',
+    month_year_end: 'July 2025',
+    description: [
+      'Architected a multi-server Nuxt.js application for workflow automation, using Tailwind CSS to prioritize UX and automating UI interactions to ensure full accessibility for users relying on screen readers.',
+      'Integrated a dedicated MCP backend in Python securely via server middleware, eliminating a class of security vulnerabilities and improving overall data processing efficiency by 60%.',
+    ],
+  },
+]
